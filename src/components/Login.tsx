4e587c63917e3fab9842329747256305358a3189
harshani <ruwanthikapemarathna@gmail.com>
2024-01-18 00:01:45 +0530

    completed login and signup pages

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,33 +1,73 @@
-function Login() {
+import axios from 'axios';
+import {useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Login:React.FC = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const navigate = useNavigate();
+
+    const login = async()=> {
+        try{
+            const response = await axios.post("http://localhost:8080/api/v1/users/login",{
+                email, password  
+        });
+
+        console.log(response);
+        navigate('/');
+        
+        setEmail(''),
+        setPassword('')
+        
+        }catch(e){
+            console.log(e);   
+        }
+        
+    }
+
+    
     return (
       <>
-      <br />
       <div>
-          <h2 className='text-center'>Login</h2><br />
-          <div className='container'>
-              <div className="mb-3 col-6 content">
-                  <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+        <form>
+      <div className='container'>
+     <div className="row">
+        <div className="card col-md-6 offset-md-3"><br />
+            <h3 className="text-center">Login</h3>
+        <div className="card-body">
+            
+              <div className="form-group">
+                  <label htmlFor="email" className="form-label">Email address</label>
+                  <input type="email"   onChange={(e)=>{setEmail(e.target.value)}} className='form-control input-item'
+                  id="email"  name="email" autoComplete="username" required/>
               </div>
-              <div className="mb-3 col-6 content">
-                  <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
-                  <input type="password" className="form-control" id="inputPassword"/>
+
+              <div className="form-group">
+                  <label htmlFor="inputPassword" className="form-label">Password</label>
+                  <input type="password" id='password'  onChange={(e)=>{setPassword(e.target.value)}}
+                    className='form-control input-item' name="password" autoComplete="current-password" required/>
                   <a href=''>Forgot password?</a>
               </div>
           </div>
           <br />
               
-          <div className="d-grid gap-2 col-4 mx-auto">
-              <button className="btn btn-primary" type="button">Login</button>
-              <span className='or'>OR</span>
-              <button className="btn btn-outline-dark" type="button">Create New Account</button>
+          <div className="d-grid gap-2 col-8 mx-auto">
+              <button className="btn btn-primary" onClick={login} type="button">Login</button>
+              <span className='text-center'>OR</span>
+              <Link className="btn btn-outline-dark" type="button" to={'/signup'} style={{marginBottom: 30}}>Create New Account</Link>
           </div>
             
+      </div>
+      </div>
+      </div>
+      </form>
       </div>
       </>
       
     )
   }
   
-  export default Login
+export default Login
   
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,37 +1,117 @@
-function Signup() {
+import axios from 'axios';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Signup:React.FC = () => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [formErrors, setFormErrors] = useState({  firstName: false, lastName: false ,email: false, password: false });
+
+    const navigate = useNavigate();
+
+    const handleFirstNameChange = (e) => {
+      setFirstName(e.target.value);
+      setFormErrors({ ...formErrors, firstName: false });
+    };
+  
+    const handleLastNameChange = (e) => {
+      setLastName(e.target.value);
+      setFormErrors({ ...formErrors, lastName: false });
+    };
+
+    const handleEmailChange = (e) => {
+      setEmail(e.target.value);
+      setFormErrors({ ...formErrors, email: false });
+    };
+  
+    const handlePasswordChange = (e) => {
+      setPassword(e.target.value);
+      setFormErrors({ ...formErrors, password: false });
+    };
+
+    const signup = async ()=> {
+        try{
+            const response = await axios.post("http://localhost:8080/api/v1/users/register",{
+                firstName, lastName, email, password  
+              
+        });
+        
+        console.log(response);
+        navigate('/login');
+        
+        setFirstName(''),
+        setLastName(''),
+        setEmail(''),
+        setPassword(''),
+        setFormErrors({ firstName: false, lastName: false, email: false, password: false });
+
+        }catch(e){
+            console.log(e);   
+        }
+    }
+
     return (
       <>
-      <br />
+    
       <div>
-          <h2 className='text-center'>Sign Up</h2><br />
-          <div className='container'>
-              <div className="mb-3 col-6 content">
-                  <label htmlFor="exampleFormControlInput1" className="form-label">Full Name</label>
-                  <input type="text" className="form-control"/>
-              </div>
-              <div className="mb-3 col-6 content">
-                  <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-              </div>
-              <div className="mb-3 col-6 content">
-                  <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
-                  <input type="password" className="form-control" id="inputPassword"/>
+        <form onSubmit={signup}>
+        {formErrors.email || formErrors.password ? (
+        <div className="alert alert-danger" role="alert">
+          Please fill in all required fields.
+        </div>
+        ) : null}
+        <div className='container'>
+            <div className="row">
+                <div className="card col-md-6 offset-md-3"><br />
+                    <h2 className="text-center">Sign Up</h2>
+                    <div className="card-body content">
+          
+                <div className="form-group">
+                  <label htmlFor="firstName" className="form-label">First Name</label>
+                  <input type="text"  onChange={handleFirstNameChange} value={firstName} id='firstName' name='firstName'
+                   className={`form-control input-item ${formErrors.firstName ? 'is-invalid' : ''}`} autoComplete="given-name" required/>
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="lastName" className="form-label">Last Name</label>
+                  <input type="text"  onChange={handleLastNameChange} value={lastName} id='lastName' name='lastName'
+                  className={`form-control input-item ${formErrors.lastName ? 'is-invalid' : ''}`} autoComplete="given-name" required />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="email" className="form-label">Email address</label>
+                  <input type="email" onChange={handleEmailChange} value= {email} name="email" id="email" 
+                   className={`form-control input-item ${formErrors.email ? 'is-invalid' : ''}`} autoComplete='usernamme' required/>
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password" className="form-label">Password</label>
+                  <input type="password" onChange={handlePasswordChange} value={password} name='password' id="password" 
+                    className={`form-control input-item ${formErrors.password ? 'is-invalid' : ''}`} autoComplete="current-password" required/>
                   <div className="col-auto">
                   <span id="passwordHelpInline" className="form-text">
                       Must be 8-20 characters long.
                   </span>
                   </div>
-              </div>
-          </div>
+                </div>
+
+            </div>
           <br />
               
-          <div className="d-grid gap-2 col-4 mx-auto">
-              <button className="btn btn-primary" type="button">Sign Up</button>
-              <span className='or'>OR</span>
-              <button className="btn btn-outline-dark" type="button">Already have an Account</button>
-          </div>
-            
+            <div className="d-grid gap-2 mx-auto">
+              <button className="btn btn-primary" type="submit">Sign Up</button>
+              <span className='text-center'>OR</span>
+              <Link className="btn btn-outline-dark" type="button" to={'/login'} style={{marginBottom: 30}}>Already have an Account</Link>
+            </div>
+         </div>   
       </div>
+      </div>
+      </form>
+      </div>
+     
       </>
     )
   }
