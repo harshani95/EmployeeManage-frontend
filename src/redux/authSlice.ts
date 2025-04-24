ca212eea7fc53f5b623ee07ef552d861c7ec6a0f
harshani <ruwanthikapemarathna@gmail.com>
2025-04-24 19:27:54 +0530

    Fixed errors in project and updated working

--- src/redux/authSlice.ts
+++ src/redux/authSlice.ts
@@ -1,18 +1,25 @@
+interface User {
+  email: string;
+  password: string;
+}
+
+type AuthAction = { type: "LOGIN_SUCCESS"; payload: User } | { type: "LOGOUT" };
+
 const initialState = {
   isAuthenticated: false,
-  user: null,
+  user: null as User | null,
 };
 
-export const loginSuccess = <T>(user: T) => ({
-  type: "LOGIN_SUCCESS",
-  payload: user,
-});
+// export const loginSuccess = <T>(user: T) => ({
+//   type: "LOGIN_SUCCESS",
+//   payload: user,
+// });
 
-export const logout = () => ({
-  type: "LOGOUT",
-});
+// export const logout = () => ({
+//   type: "LOGOUT",
+// });
 
-const authReducer = (state = initialState, action) => {
+const authReducer = (state = initialState, action: AuthAction) => {
   switch (action.type) {
     case "LOGIN_SUCCESS":
       return {
